1ab3d2f4767e86a025ff57cc0b66b9e16fa6bdea
Milutin <grobar.e@gmail.com>
2022-10-02 23:20:27 +0200

    Keylogger - simple

--- js-exercise/script.js
+++ js-exercise/script.js
@@ -1,1 +1,7 @@
-    
+const inputBox = document.querySelector("#init");
+const outBox = document.querySelector("#output");
+
+inputBox.addEventListener(
+  "keydown",
+  (Event) => (outBox.textContent = `You pressed ${Event.key}.`)
+);
