d19b55dfce80d8d4d0da10a49b0089b5658b5e8f
Milutin <grobar.e@gmail.com>
2022-10-03 15:29:27 +0200

    canvas ctx.draw()

--- js-exercise/script.js
+++ js-exercise/script.js
@@ -1,31 +1,31 @@
-const names = [
-  "Chris",
-  "Li Kang",
-  "Anne",
-  "Francesca",
-  "Mustafa",
-  "Tina",
-  "Bert",
-  "Jada",
-];
-const para = document.createElement("p");
+// const names = [
+//   "Chris",
+//   "Li Kang",
+//   "Anne",
+//   "Francesca",
+//   "Mustafa",
+//   "Tina",
+//   "Bert",
+//   "Jada",
+// ];
+// const para = document.createElement("p");
 
-// Add your code here
+// // Add your code here
 
-function ranInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+// function ranInt(min, max) {
+//   return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
 
-function output(names) {
-  return names[ranInt(0, names.length - 1)];
-}
+// function output(names) {
+//   return names[ranInt(0, names.length - 1)];
+// }
 
-// without output function
+// // without output function
 
-// para.textContent = names[ranInt(0, names.length - 1)];
+// // para.textContent = names[ranInt(0, names.length - 1)];
 
-para.textContent = output(names);
+// para.textContent = output(names);
 
-const section = document.querySelector("body");
+// const section = document.querySelector("body");
 
-section.appendChild(para);
+// section.appendChild(para);
