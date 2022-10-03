16d26b506026b641aa999cb3ae731b592da47d3a
Milutin <grobar.e@gmail.com>
2022-10-03 13:26:56 +0200

    js practice continue

--- js-exercise/script.js
+++ js-exercise/script.js
@@ -1,24 +1,7 @@
-const names = [
-  "Chris",
-  "Li Kang",
-  "Anne",
-  "Francesca",
-  "Mustafa",
-  "Tina",
-  "Bert",
-  "Jada",
-];
+const input = document.querySelector("#input");
+const output = document.querySelector("#output");
 
-const para = document.createElement("p");
-
-// Add your code here
-function ranit(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function chooseName(names) {
-  return names[ranit(0, names.length - 1)];
-}
-
-para.textContent = chooseName(names);
-document.body.appendChild(para);
+input.addEventListener(
+  "keydown",
+  (Event) => (output.textContent = `You pressed "${Event.key}"`)
+);
