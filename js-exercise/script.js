5a6fa8a1875d2a788c7aba37b7bc7e9457b0af53
Milutin <grobar.e@gmail.com>
2022-10-03 15:07:15 +0200

    creating + appending new elements

--- js-exercise/script.js
+++ js-exercise/script.js
@@ -1,17 +1,31 @@
-const x = 1;
+const names = [
+  "Chris",
+  "Li Kang",
+  "Anne",
+  "Francesca",
+  "Mustafa",
+  "Tina",
+  "Bert",
+  "Jada",
+];
+const para = document.createElement("p");
 
-function a() {
-  const y = 2;
-  output(y);
+// Add your code here
+
+function ranInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function b() {
-  const z = 3;
-  output(z);
+function output(names) {
+  return names[ranInt(0, names.length - 1)];
 }
 
-function output(value) {
-  const para = document.createElement("p");
-  document.body.appendChild(para);
-  para.textContent = `Value: ${value}`;
-}
+// without output function
+
+// para.textContent = names[ranInt(0, names.length - 1)];
+
+para.textContent = output(names);
+
+const section = document.querySelector("body");
+
+section.appendChild(para);
