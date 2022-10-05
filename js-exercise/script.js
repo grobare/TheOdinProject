534624b9fdf068df6c4764f02615ee591b12eea7
Milutin <grobar.e@gmail.com>
2022-10-05 15:40:28 +0200

    lets go

--- js-exercise/script.js
+++ js-exercise/script.js
@@ -1,16 +1,1 @@
-function pow(x, y) {
-  let sum = x;
-  for (let index = 1; index < y; index++) {
-    sum *= x;
-  }
-  return sum;
-}
 
-let x = prompt("Please type x:");
-let y = prompt("Please type y:");
-
-if (y < 1) {
-  alert(`${y} is illegal and you know it!!!`);
-} else {
-  alert(pow(x, y));
-}
