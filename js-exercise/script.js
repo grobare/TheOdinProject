f20a5d4a83e8699c083124dff31bba7f4b32c909
Milutin <grobar.e@gmail.com>
2022-10-10 20:33:03 +0200

    fizzBuzz

--- js-exercise/script.js
+++ js-exercise/script.js
@@ -1,26 +1,14 @@
-function add7(num) {
-  return num + 7;
-}
-
-function multiply(num1, num2) {
-  return num1 * num2;
-}
-
-function capitalize(string) {
-  let newString = "";
-  for (let index = 0; index < string.length; index++) {
-    index === 0
-      ? (newString += string[index].toUpperCase())
-      : (newString += string[index].toLowerCase());
-    //     if (index === 0) {
-    //       newString += string[index].toUpperCase();
-    //     } else {
-    //       newString += string[index].toLowerCase();
-    //     }
+function fizzBuzz() {
+  let num = prompt("Enter you value:");
+  for (let index = 1; index <= num; index++) {
+    if (index % 3 === 0 && index % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (index % 3 === 0) {
+      console.log("Fizz");
+    } else if (index % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(index);
+    }
   }
-  return newString;
-}
-
-function lastLetter(string) {
-  return string[string.length - 1];
 }
