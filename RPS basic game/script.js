33e9488bee497ee5d8474d07cf4c869febe4b007
Milutin <grobar.e@gmail.com>
2022-10-23 00:58:03 +0200

    basic gameplay in console added

--- RPS basic game/script.js
+++ RPS basic game/script.js
@@ -1,13 +1,3 @@
-//player must make a choice
-//random number
-//compare func
-//player decision based on input
-
-function playerChoose() {
-  const answer = prompt("Enter your choice!");
-  return answer;
-}
-
 function getComputerChoice() {
   const randInt = Math.floor(Math.random() * 3) + 1;
   if (randInt === 1) {
@@ -19,17 +9,24 @@ function getComputerChoice() {
   if (randInt === 3) {
     return "scissors";
   }
-  return;
+  return "Wrong input.";
 }
 
-function playRound(PC, human) {
-  if (PC === "rock" && human === "scissors") {
-    console.log("You lose! Rock beats scissors!");
-  } else if (PC === "paper" && human === "rock") {
-    console.log("You lose! Paper beats rock!");
-  } else if (PC === "scissors" && human === "paper") {
-    console.log("You lose! Scissors beats paper!");
+function playRound() {
+  const PC = getComputerChoice();
+  const human = prompt("Enter your choice!");
+
+  if (human === "paper" || human === "rock" || human === "scissors") {
+    if (PC === "rock" && human === "scissors") {
+      console.log("You lose! Rock beats scissors!");
+    } else if (PC === "paper" && human === "rock") {
+      console.log("You lose! Paper beats rock!");
+    } else if (PC === "scissors" && human === "paper") {
+      console.log("You lose! Scissors beats paper!");
+    } else {
+      console.log("You win!");
+    }
   } else {
-    console.log("You win!");
+    console.log("Wrong entry.");
   }
 }
